7af56a9f5940642ca1d78605d982258d7ac686e8
Steven Mask <mnicpt@gmail.com>
2014-03-17 11:02:23 -0500

    Refactored port code

--- web.js
+++ web.js
@@ -3,6 +3,9 @@ var express = require("express"),
 
 app.get('/', function(request, response) {
   response.send('<h1>Currently Under Construction</h1>Call 217-369-2921 for information on Worship Service location and time and current events to be involved in.<br>Sorry for any inconvenience.');
-}).listen(8080, function() {
-    console.log("Listening on port 8080");
+});
+
+var port = process.env.PORT || 8080;
+app.listen(port, function() {
+  console.log("Listening on " + port);
 });
