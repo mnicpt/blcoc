5ae5c2802e6a68e32a5b6df968c8a09b0940c7b0
Steven Mask <mnicpt@gmail.com>
2014-03-17 10:49:55 -0500

    Fixed compile error

--- web.js
+++ web.js
@@ -2,6 +2,7 @@ var express = require("express"),
     app = express.createServer(); 
 
 app.get('/', function(request, response) {
-  res.writeHead(200, {'Content-Type': 'text/html'});
-  res.end('<h1>Currently Under Construction</h1>Call 217-369-2921 for information on Worship Service location and time and current events to be involved in.<br>Sorry for any inconvenience.');
-}).listen(8080, function(){console.log("Listening on port 8080"});
+  response.send('<h1>Currently Under Construction</h1>Call 217-369-2921 for information on Worship Service location and time and current events to be involved in.<br>Sorry for any inconvenience.');
+}).listen(8080, function() {
+    console.log("Listening on port 8080");
+};
