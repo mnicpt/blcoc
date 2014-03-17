8d7b178127169045d055d6ed877ec30a612ccef3
Steven Mask <mnicpt@gmail.com>
2014-03-17 10:58:54 -0500

    Fixed deprecated method in express

--- web.js
+++ web.js
@@ -1,5 +1,5 @@
 var express = require("express"),
-    app = express.createServer(); 
+    app = express(); 
 
 app.get('/', function(request, response) {
   response.send('<h1>Currently Under Construction</h1>Call 217-369-2921 for information on Worship Service location and time and current events to be involved in.<br>Sorry for any inconvenience.');
