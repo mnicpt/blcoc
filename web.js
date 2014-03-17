ceddbded329e3b6aa047cf57bdd73031e1dd1466
Steven Mask <mnicpt@gmail.com>
2014-03-17 10:54:39 -0500

    Fixed syntax error

--- web.js
+++ web.js
@@ -5,4 +5,4 @@ app.get('/', function(request, response) {
   response.send('<h1>Currently Under Construction</h1>Call 217-369-2921 for information on Worship Service location and time and current events to be involved in.<br>Sorry for any inconvenience.');
 }).listen(8080, function() {
     console.log("Listening on port 8080");
-};
+});
