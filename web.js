73d5483d28fe3befc0c3eed836f1b755a2b894bf
Steven Mask <mnicpt@gmail.com>
2014-03-17 11:12:02 -0500

    Fixing deploy

--- web.js
+++ web.js
@@ -2,7 +2,7 @@ var express = require("express"),
     app = express(); 
 
 app.get('/', function(request, response) {
-  response.send('<h1>Currently Under Construction</h1>Call 217-369-2921 for information on Worship Service location and time and current events to be involved in.<br>Sorry for any inconvenience.');
+  response.send('Under Construction.');
 });
 
 var port = process.env.PORT || 8080;
