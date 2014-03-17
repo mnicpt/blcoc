f607dc6b59197cecdb4c5462f7de3990678bb708
Steven Mask <mnicpt@gmail.com>
2014-03-17 10:29:19 -0500

    Fixed url in web.js

--- web.js
+++ web.js
@@ -3,6 +3,6 @@ var http = require('http');
 http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/plain'});
   res.end('<h1>Currently Under Construction</h1>Call 217-369-2921 for information on Worship Service location and time and current events to be involved in.  Ask for Steve.  Sorry for any inconvenience.');
-}).listen(8080, '/');
+}).listen(8080, 'blcoc.net');
 
 console.log('Server running...');
