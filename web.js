a9e925f20de64be3ed1216d2f4e044c84917200e
Steven Mask <mnicpt@gmail.com>
2014-03-17 10:46:42 -0500

    Added express

--- web.js
+++ web.js
@@ -1,8 +1,7 @@
-var http = require('http');
+var express = require("express"),
+    app = express.createServer(); 
 
-http.createServer(function (req, res) {
+app.get('/', function(request, response) {
   res.writeHead(200, {'Content-Type': 'text/html'});
   res.end('<h1>Currently Under Construction</h1>Call 217-369-2921 for information on Worship Service location and time and current events to be involved in.<br>Sorry for any inconvenience.');
-}).listen(8080,'/');
-
-console.log('Server running...');
+}).listen(8080, function(){console.log("Listening on port 8080"});
