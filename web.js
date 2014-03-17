6e90a93f44a8f2e8c57f7e2be0e570dd9ed962cc
Steven Mask <mnicpt@gmail.com>
2014-03-17 11:33:56 -0500

    Site is up

--- web.js
+++ web.js
@@ -2,7 +2,7 @@ var express = require("express"),
     app = express(); 
 
 app.get('/', function(req, res) {
-  res.send('Under Construction.');
+  res.send('<center><h1>Under Construction.</h1>Contact Steve at 217-369-2921 for directions and time of Sunday worship service.<br>Sorry for any inconvenience as we update our site.</center>');
 });
 
 var port = process.env.PORT || 8080;
