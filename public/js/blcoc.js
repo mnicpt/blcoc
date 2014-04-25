8a557887ac8e5cb20fc050b303048c3fafdba3d9
Steven Mask <mnicpt@gmail.com>
2014-04-24 23:29:56 -0500

    Added angular functionality

--- public/js/blcoc.js
+++ public/js/blcoc.js
@@ -1,6 +1,7 @@
 angular.module('blcoc', ['ngRoute'])
 
 .config(function($routeProvider, $locationProvider) {
+  $locationProvider.html5Mode(true);
   $routeProvider
     .when('/home', {
       controller:'HomeCtrl',
