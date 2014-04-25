02d582c61c782aa8b8cab3eabdabf588c0cad516
Steven Mask <mnicpt@gmail.com>
2014-04-24 22:26:10 -0500

    Added angular functionality

---
+++ public/js/blcoc.js
@@ -0,0 +1,50 @@
+angular.module('blcoc', ['ngRoute'])
+
+.config(function($routeProvider, $locationProvider) {
+  $locationProvider.html5Mode(true);
+  $routeProvider
+    .when('/home', {
+      controller:'HomeCtrl',
+      templateUrl:'home.html'
+    })
+    .when('/about', {
+      controller:'AboutCtrl',
+      templateUrl:'about.html'
+    })
+    .when('/events', {
+      controller:'EventsCtrl',
+      templateUrl:'events.html'
+    })
+    .when('/programs', {
+      controller:'ProgramsCtrl',
+      templateUrl:'programs.html'
+    })
+    .when('/groups', {
+      controller:'GroupsCtrl',
+      templateUrl:'groups.html'
+    })
+    .otherwise({
+      controller:'HomeCtrl',
+      templateUrl:'home.html'
+    });
+})
+
+.controller('HomeCtrl', function($scope) {
+    
+})
+
+.controller('AboutCtrl', function($scope) {
+
+})
+
+.controller('EventsCtrl', function($scope) {
+    
+})
+
+.controller('ProgramsCtrl', function($scope) {
+   
+})
+
+.controller('GroupsCtrl', function($scope) {
+  
+});
