73c168da3ef426f58114a9aff0b243e01d1ec659
Steven Mask <mnicpt@gmail.com>
2014-04-25 00:19:51 -0500

    Moved html files.

--- public/js/blcoc.js
+++ public/js/blcoc.js
@@ -1,7 +1,7 @@
 angular.module('blcoc', ['ngRoute'])
 
 .config(function($routeProvider, $locationProvider) {
-  $locationProvider.html5Mode(true);
+  $locationProvider.html5Mode(false);
   $routeProvider
     .when('/home', {
       controller:'HomeCtrl',
